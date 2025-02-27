1461381b00933afc3263f8c215acc6d1993b87e2
Andromedov <andromedov.mod@gmail.com>
2025-02-28 00:45:04 +0200

    [NotReady] First commit of lab2

---
+++ Lab2/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import static java.lang.Math.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Apartment u1 = new Apartment(1, 1, 10.1, 1, 2, "Shevchenka 13");
+        Apartment u2 = new Apartment(2, 2, 23.4, 1, 3, "Shevchenka 13");
+        Apartment u3 = new Apartment(3, 3, 64.3, 1, 3, "Shevchenka 13");
+        Apartment u4 = new Apartment(4, 4, 32.7, 1, 2, "Shevchenka 13");
+    }
+}
