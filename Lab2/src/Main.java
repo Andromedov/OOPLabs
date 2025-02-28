68c83506774a35a3a81685b92bb91c52740136d6
bryndioleksandr <s.v.bryndo@gmail.com>
2025-02-28 15:27:58 +0200

    added filter for rooms & area

--- Lab2/src/Main.java
+++ Lab2/src/Main.java
@@ -2,10 +2,40 @@ import java.util.*;
 import static java.lang.Math.*;
 
 public class Main {
+    private static Apartment[] apartments = new Apartment[10];
     public static void main(String[] args) {
-        Apartment u1 = new Apartment(1, 1, 10.1, 1, 2, "Shevchenka 13");
-        Apartment u2 = new Apartment(2, 2, 23.4, 1, 3, "Shevchenka 13");
-        Apartment u3 = new Apartment(3, 3, 64.3, 1, 3, "Shevchenka 13");
-        Apartment u4 = new Apartment(4, 4, 32.7, 1, 2, "Shevchenka 13");
+        Scanner scanner = new Scanner(System.in);
+        apartments[0] = new Apartment(1, 101, 45.5, 3, 2, "Вулиця Центральна");
+        apartments[1] = new Apartment(2, 102, 50.0, 4, 3, "Вулиця Зелена");
+        apartments[2] = new Apartment(3, 103, 38.2, 2, 1, "Вулиця Синя");
+        apartments[3] = new Apartment(4, 104, 60.7, 5, 3, "Проспект Парковий");
+        apartments[4] = new Apartment(5, 105, 42.3, 1, 2, "Вулиця Дубова");
+        apartments[5] = new Apartment(6, 106, 55.4, 3, 2, "Вулиця Соснова");
+        apartments[6] = new Apartment(7, 107, 47.1, 6, 1, "Бульвар Західний");
+        apartments[7] = new Apartment(8, 108, 63.0, 7, 4, "Вулиця Річкова");
+        apartments[8] = new Apartment(9, 109, 49.8, 2, 2, "Вулиця Вербова");
+        apartments[9] = new Apartment(10, 110, 52.6, 4, 3, "Вулиця Кленова");
+
+        System.out.print("Квартири з кількістю кімнат: ");
+        double scanRooms = scanner.nextInt();
+        printByRooms(scanRooms);
+        System.out.print("Квартири з площею: ");
+        double scanArea = scanner.nextDouble();
+        printByArea(scanArea);
+    }
+
+    public static void printByRooms(double scanRooms) {
+        for (Apartment apartment : apartments) {
+            if (apartment.getArea() == scanRooms) {
+                System.out.println("По кількості кімнат: " + apartment);
+            }
+        }
+    }
+    public static void printByArea(double scanArea) {
+        for (Apartment apartment : apartments) {
+            if (apartment.getArea() > scanArea) {
+                System.out.println("По площі: " + apartment);
+            }
+        }
     }
 }
