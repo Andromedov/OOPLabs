75d17ca25f3f4e01f6b95c642d25d0663c13b58e
Andromedov <andromedov.mod@gmail.com>
2025-04-05 16:03:08 +0300

    Lab7 v1

---
+++ Lab7/src/Apartment.java
@@ -0,0 +1,67 @@
+public class Apartment extends Property {
+    private Double area;
+    private Integer rooms;
+    private static Integer counter = 0;
+
+    public Apartment() {
+        super();
+        this.area = 0.0;
+        this.rooms = 1;
+    }
+
+    public Apartment(Integer number, Double area) {
+        super(++counter, number, 1, "");
+        this.area = area;
+        this.rooms = 1;
+    }
+
+    public Apartment(Integer number, Double area, Integer rooms) {
+        super(++counter, number, 1, "");
+        this.area = area;
+        this.rooms = rooms;
+    }
+
+    public Apartment(Integer id, Integer number, Double area, Integer floor, Integer rooms, String street) {
+        super(id, number, floor, street);
+        this.area = area;
+        this.rooms = rooms;
+        counter++;
+    }
+
+    public Apartment(Apartment other) {
+        super(other);
+        this.area = other.area;
+        this.rooms = other.rooms;
+        counter++;
+    }
+
+    public Double getArea() {
+        return area;
+    }
+
+    public void setArea(Double area) {
+        this.area = area;
+    }
+
+    public Integer getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(Integer rooms) {
+        this.rooms = rooms;
+    }
+
+    @Override
+    public String getPropertyType() {
+        return "Apartment";
+    }
+
+    @Override
+    public String toString() {
+        return "Apartment{" +
+                super.toString() +
+                ", area=" + area +
+                ", rooms=" + rooms +
+                '}';
+    }
+}
