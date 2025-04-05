4e294a90d068101f4f5d1a185568f079c7bbb0ea
Andromedov <andromedov.mod@gmail.com>
2025-04-05 15:32:54 +0300

    pulled main

--- Lab6/src/Main.java
+++ Lab6/src/Main.java
@@ -6,16 +6,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        HotelRoom hotelRoom = new HotelRoom(1, 101, 2, 2, "Hotel Street");
-        System.out.println("Hotel Room: " + hotelRoom);
+        HotelRoom hotelRoom = new HotelRoom(1, 101, 2, 2, "Вулиця 1");
+        System.out.println("HotelRoom: " + hotelRoom);
 
-        Apartment apt = new Apartment(1, 103, 38.2, 2, 2, "Apartment Street");
+        Apartment apt = new Apartment(1, 103, 38.2, 2, 2, "Вулиця 2");
         System.out.println("Apartment: " + apt);
 
-        properties[0] = new Apartment(1, 101, 45.5, 3, 2, "Central Street");
-        properties[1] = new HotelRoom(2, 102, 2, 3, "Hotel Avenue");
-        properties[2] = new Apartment(3, 103, 38.2, 2, 1, "Blue Street");
-        properties[3] = new HotelRoom(4, 104, 4, 2, "Resort Road");
+        properties[0] = new Apartment(1, 101, 45.5, 3, 2, "Вулиця 3");
+        properties[1] = new HotelRoom(2, 102, 2, 3, "Вулиця 4");
+        properties[2] = new Apartment(3, 103, 38.2, 2, 1, "Вулиця 5");
+        properties[3] = new HotelRoom(4, 104, 4, 2, "Вулиця 6");
 
         System.out.println("\nУсе майно:");
         for (Property property : properties) {
